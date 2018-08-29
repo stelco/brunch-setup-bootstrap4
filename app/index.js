5c61c371193fbe243c072ac5d5f88793dad7e205
Steven Collins <stec74@gmail.com>
2018-08-29 16:19:21 +0100

    header and footer responsive tweaking

--- app/index.js
+++ app/index.js
@@ -6,8 +6,18 @@ import 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
+
   console.log('Initialized app');
   if ($('btn')) {
     console.log('jQuery works')
   }
+  
+  /*collapse (accordian) chevron*/
+  $('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+  
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
 });
